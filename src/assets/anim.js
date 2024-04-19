8191be3179f0bcf71bd9ccf51eea6b6128507fe7
SegoleneLazarus <seg.laz@ik.me>
2024-04-19 21:38:16 +0200

    pétard ya un changement

--- src/assets/anim.js
+++ src/assets/anim.js
@@ -1,14 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var image = document.getElementById('soleil');
   
-    // Ajoute un gestionnaire d'événements pour le clic sur l'image
     document.addEventListener('click', function() {
-      // Agrandit l'image en ajoutant la classe 'large'
       image.classList.add('large');
   
-      // Utilise setTimeout pour que l'image disparaisse après 2 secondes
       setTimeout(function() {
-        // Ajoute une classe pour cacher l'image
         image.classList.add('hidden');
       }, 2000);
     });
